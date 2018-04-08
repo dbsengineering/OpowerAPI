29a2dc11bcaf613f844f9c63ab039071f0a74f9f
DbsEngineering <piranna56@yahoo.fr>
2018-04-08 11:42:51 +0200

    base TP

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opowerApiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the opowerApiApp
+ */
+angular.module('opowerApiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opowerApiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the opowerApiApp
+ */
+angular.module('opowerApiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
